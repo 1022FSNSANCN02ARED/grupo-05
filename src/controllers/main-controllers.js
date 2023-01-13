5656226bcb19c10c2a874714e76eaf65699dd930
lautarosaya <lautarosaya0012@gmail.com>
2023-01-13 20:03:54 -0300

    corrige rutas inssues

--- src/controllers/main-controllers.js
+++ src/controllers/main-controllers.js
@@ -5,11 +5,9 @@ module.exports = {
     res.render("index");
   },
   login: (req, res) => {
-    __dirname +
-      res.sendFile(path.resolve(__dirname, "../views/users/login.html"));
+    res.sendFile(path.resolve(__dirname, "../views/users/login.html"));
   },
   register: (req, res) => {
-    __dirname +
-      res.sendFile(path.resolve(__dirname, "../views/users/register.html"));
+    res.sendFile(path.resolve(__dirname, "../views/users/register.html"));
   },
 };

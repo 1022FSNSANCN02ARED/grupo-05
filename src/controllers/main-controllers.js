05d2a7822e772eecce5b04673245907145aac3ca
atarico9 <atarico9@gmail.com>
2023-01-14 21:35:54 -0300

    crea template engine en en el INDEX

--- src/controllers/main-controllers.js
+++ src/controllers/main-controllers.js
@@ -1,8 +1,40 @@
 const path = require("path");
 
+let productos = [
+  {
+    nombre: "Cafetera Moulinex",
+    oferta: "40% OFF",
+    precio: 6770,
+    img: "../public/img/productos/img-cafetera-moulinex.jpg",
+    alt: "cafetera-moulinex",
+  },
+  {
+    nombre: "MacBook Pro 2019",
+    oferta: "20% off",
+    precio: 230000,
+    img: "../public/img/productos/img-macbook-pro-2019.jpg",
+    alt: "macbook-pro-2019",
+  },
+  {
+    nombre: "Samsung Galaxy S10",
+    oferta: "10% OFF",
+    precio: 70500,
+    img: "../public/img/productos/img-samsung-galaxy-s10.jpg",
+    alt: "samsung-galaxy-s10",
+  },
+  {
+    nombre: "SmartTv Samsung",
+    oferta: "5% OFF",
+    precio: 230000,
+    img: "../public/imgs/productos/img-tv-samsung-smart.jpg",
+    alt: "tv-samsung-smart",
+  },
+];
+
+
 module.exports = {
   home: (req, res) => {
-    res.render("index");
+    res.render("index", { productos });
   },
   login: (req, res) => {
     res.render("./users/login");

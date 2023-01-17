eb68c8bb041dfdc719daa1ebf7fa898aff3229b9
lautarosaya <lautarosaya0012@gmail.com>
2023-01-16 22:21:32 -0300

    editar producto

--- src/controllers/products-controllers.js
+++ src/controllers/products-controllers.js
@@ -6,4 +6,8 @@ module.exports = {
     const id = req.params.id;
     res.render("product", { productos, id });
   },
+  editar_producto: (req, res) => {
+    const id = req.params.id;
+    res.render("products/editar-producto", { productos, id });
+  },
 };
--- src/router/products-router.js
+++ src/router/products-router.js
@@ -5,5 +5,6 @@ const productsControllers = require("../controllers/products-controllers.js");
 
 /* http:// localhost:(Port)/products */
 router.get("/product/:id", productsControllers.product);
+router.get("/editar-producto", productsControllers.editar_producto);
 
 module.exports = router;

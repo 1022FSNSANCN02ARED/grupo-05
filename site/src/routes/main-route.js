2c909edfe0759b0a7f76367dc6f874c8cd33e7d7
lautarosaya <lautarosaya0012@gmail.com>
2023-05-15 19:54:52 -0300

    arreglando error

--- site/src/routes/main-route.js
+++ site/src/routes/main-route.js
@@ -4,6 +4,7 @@ const mainController = require("../controllers/main-controller");
 const userRoute = require("./user-route");
 const productRoute = require("./product-router");
 const adminRoute = require("./admin-route");
+const cartRoute = require("./cart-route");
 const apiUsersRouter = require("./api/userAPIrouter");
 const apiProductsRouter = require("./api/productsAPIrouter");
 const apiCategoryRouter = require("./api/categoryAPIrouter");
@@ -17,6 +18,8 @@ router.use("/user", userRoute);
 router.use("/product", productRoute);
 
 router.use("/admin", adminRoute);
+
+router.use("/cart", cartRoute);
 
 router.use("/api/users", apiUsersRouter);
 router.use("/api/products", apiProductsRouter);

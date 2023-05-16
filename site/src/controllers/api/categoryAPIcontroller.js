349e8cf069590a769d0d7b8c13343c03b71b1d7e
atarico9 <atarico9@gmail.com>
2023-05-15 23:58:38 -0300

    cambia controladores y fetch

--- site/src/controllers/api/categoryAPIcontroller.js
+++ site/src/controllers/api/categoryAPIcontroller.js
@@ -2,23 +2,21 @@ const { Category } = require("../../../database/models");
 const pageSize = 5;
 
 const categoryAPIController = {
-  list: async (req, res) => {
-    const page = Number(req.query.page) || 1;
-    const offset = (page - 1) * pageSize;
-
-    const { count, rows } = await Category.findAndCountAll({
-      limit: pageSize,
-      offset: offset,
-    });
-    res.json({
-      meta: {
-        status: 200,
-        total: count,
-        url: req.originalUrl,
-      },
-      data: rows,
+  list: (req, res) => {
+    Category.findAll().then((Category) => {
+      let respuesta = {
+        meta: {
+          status: 200,
+          total: Category.length,
+          url: "api/category",
+        },
+        data: Category,
+      };
+      res.json(respuesta);
     });
   },
+  /* const page = Number(req.query.page) || 1;
+  const offset = (page - 1) * pageSize; */
 
   categoryDetail: async (req, res) => {
     const category = await Category.findByPk(req.params.id);
